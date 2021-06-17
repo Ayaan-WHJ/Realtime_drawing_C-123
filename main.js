828bbe5e16b94eb1606516e11f1c91ae702bddf8
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-06-17 16:35:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup()
 {
     video = createCapture(VIDEO);
@@ -12,6 +17,10 @@ function setup()
 function draw()
 {
     background('#0000FF');
+    document.getElementById("square_side").innerHTML = "The radius of the circle will be = " +difference + "px";
+    fill('rgb(255, 0, 0)')
+    stroke('rgb(255, 0, 0)');
+    circle(noseX,noseY,difference);
 }
 
 function modelLoaded()
@@ -23,5 +32,13 @@ function gotPoses(results)
 {
     if (results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose X = " + noseX + "nose Y = " + noseY );
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
     }
 }
